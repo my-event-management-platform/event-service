319825eb0ca3ce8baf2b81b76070433b7a1dcf27
Lev <leva.averin@yandex.ru>
2023-06-28 17:59:46 +0300

    Added event model

---
+++ src/main/java/com/eventmanagement/eventservice/model/Event.java
@@ -0,0 +1,19 @@
+package com.eventmanagement.eventservice.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Data
+@Document
+public class Event {
+    @Id
+    private String id;
+    private String title;
+    private String text;
+    private String location;
+    private Instant datetime;
+    private Integer capacity;
+}
